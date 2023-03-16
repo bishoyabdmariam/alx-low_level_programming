94039370d9b63e6b9235dc0593c167f66e785288
bishoy <bishoyabdo236@gamil.com>
2023-03-16 15:17:23 -0700

    MY FIRST COMMIT

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,8 +8,8 @@
 
 
 
-int main() 
+int main(void) 
 {
- 	 printf("\"Programming is like building a multilingual puzzle");
-  	return 0;
+ 	puts("\"Programming is like building a multilingual puzzle");
+  	return (0);
 }
